80ee7a3f43010215e13511ba19425fed2707803a
Varun Joshi <72180614+VarunJoshi10@users.noreply.github.com>
2024-06-16 19:10:19 +0530

    Update settings.ts

--- src/state/settings.ts
+++ src/state/settings.ts
@@ -1,15 +1,15 @@
 import { MyStateCreator } from './store';
 
 export type SettingsSlice = {
-  openAIKey: string | null;
+  zynapseKey: string | null;
   selectedModel: string;
   actions: {
     update: (values: Partial<SettingsSlice>) => void;
   };
 };
 export const createSettingsSlice: MyStateCreator<SettingsSlice> = (set) => ({
-  openAIKey: null,
-  selectedModel: 'gpt-3.5-turbo',
+  zynapseKey: null,
+  selectedModel: 'mistral',
   actions: {
     update: (values) => {
       set((state) => {
